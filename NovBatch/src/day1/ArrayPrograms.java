656c648977cecdf852bf67809af27b8734164d06
karthikeyan M <61132641+kathi1511@users.noreply.github.com>
2025-11-19 18:54:34 +0530

    Added the remove element array code

--- NovBatch/src/day1/ArrayPrograms.java
+++ NovBatch/src/day1/ArrayPrograms.java
@@ -1,9 +1,37 @@
 package day1;
 
 public class ArrayPrograms {
+	
+	
+	public static void  removeElement() {
+		
+		int a []= {5,10,15,20,25};
+		int size = a.length;
+		System.out.println("Array size:"+size);
+		
+		for (int array : a) {
+			System.out.print(array+" ");
+			
+		}
+
+		
+		int newArray[]= new int[size-1];
+		
+		for (int i = 0; i <size-1; i++) {
+			newArray[i]= a[i];
+			
+		}
+		System.out.println();
+		
+		for (int arr : newArray) {
+			System.out.print(arr+" ");
+			
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		removeElement();
 
 	}
 

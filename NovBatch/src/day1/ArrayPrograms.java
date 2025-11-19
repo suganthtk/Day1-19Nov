cd34562030ca491d9f3b221a1e04cf2aff8e9230
suganthtk <suganthtk@gmail.com>
2025-11-19 19:12:06 +0530

    Added FindDuplicate element  array code

--- NovBatch/src/day1/ArrayPrograms.java
+++ NovBatch/src/day1/ArrayPrograms.java
@@ -29,9 +29,28 @@ public class ArrayPrograms {
 		}
 	}
 
+	
+	public static void FindDuplicate() {
+		 int[] arr = {1, 5, 2, 3, 5, 1, 7, 2, 9};
+		 System.out.print("Duplicate ELement is:" );
+		 for (int i = 0; i < arr.length; i++) {
+			 for (int k = i+1; k < arr.length; k++) {
+				 if (arr[i]==arr[k]) {
+					 
+					 System.out.print(arr[i] +", ");
+					 
+			}	 
+				
+			}
+		 }
+	}
+			
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		removeElement();
+		FindDuplicate();
+		
 
 	}
 
